d77875b267e55cddcacff018a5cd5f1716f79f39
moti9 <motiky.iiitag@gmail.com>
2023-03-10 14:26:52 +0530

    "10032023"

--- py/pieChart.py
+++ py/pieChart.py
@@ -1,12 +1,13 @@
 import matplotlib.pyplot as plt
 
 # Pie chart, where the slices will be ordered and plotted counter-clockwise:
-labels = ['Work1', 'Work2', 'Work3', 'Work4']
-sizes = [15, 30, 45, 10]
+labels = ['Work1', 'Work2', 'Work3', 'Work4','Work5']
+sizes = [15, 25, 35, 10,15]
 
 fig1, ax1 = plt.subplots()
 ax1.pie(sizes, labels=labels, autopct='%1.1f%%',
         shadow=True, startangle=90)
 ax1.axis('equal')  # Equal aspect ratio ensures that pie is drawn as a circle.
 
-plt.show()
+# plt.show()
+plt.savefig('piechart.png',transparent=True)
---
+++ py/youtubeVidDownload.py
@@ -0,0 +1,8 @@
+import pytube
+
+url = input("Enter the uls: ")
+
+path = "D"
+
+# pytube.YouTube(url).streams.get_highest_resolution.download(path)
+# pytube.YouTube(url).streams
